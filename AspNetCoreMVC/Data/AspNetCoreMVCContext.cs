98b49879cb0c38c1fb3142a33ce3b674abcc1383
Siva Alamalakala <siva.alamalakala@realpage.com>
2023-06-28 22:35:52 +0530

    AspNet Core with MVC

---
+++ AspNetCoreMVC/Data/AspNetCoreMVCContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using AspNetCoreMVC.Models;
+
+namespace AspNetCoreMVC.Data
+{
+    public class AspNetCoreMVCContext : DbContext
+    {
+        public AspNetCoreMVCContext (DbContextOptions<AspNetCoreMVCContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<AspNetCoreMVC.Models.Movie> Movie { get; set; } = default!;
+    }
+}
